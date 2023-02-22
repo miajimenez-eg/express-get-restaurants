3338a8210c0272b86b7c806a3c677c70e1fe8bc8
Mia Jimenez <miajimenez@expediagroup.com>
2023-02-22 14:32:32 +0000

    add express routes for create update and delete

--- server.js
+++ server.js
@@ -11,11 +11,40 @@ app.get('/restaurants', async (req, res) => {
     res.json(allRestaurants);
 });
 
-// GET request for "/restaurants/:id"
+// PT2 GET request for "/restaurants/:id"
 app.get('/restaurants/:id', async (req, res) => {
     const restaurant = await Restaurant.findByPk(req.params.id);
     res.json(restaurant)
 })
+
+// PT3 create express routes
+app.use(express.json())
+app.post('/restaurants', async (req, res) => {
+    req.body = {name: "Vapiano", location: "Covent Garden", cuisine: "Italian"}
+    const newRestaurant = req.body;
+    const createNewRest = await Restaurant.create(newRestaurant);
+    const allRestaurants = await Restaurant.findAll()
+    res.json(allRestaurants);
+})
+
+app.use(express.json())
+app.put('/restaurants/:id', async (req, res) => {
+    let id = req.params.id;
+    req.body = {name: "Nandos", location: "London", cuisine: "Brazilian"}
+    const newRestaurant = req.body;
+    const updateRestaurant = await Restaurant.update(newRestaurant, { where: { id: id } });
+    const allRestaurants = await Restaurant.findAll();
+    res.json(allRestaurants);
+})
+
+app.use(express.json())
+app.delete('/restaurants/:id', async (req, res) => {
+    let id = req.params.id;
+    const deleteRestaurant = await Restaurant.destroy({ where: { id: id } });
+    const allRestaurants = await Restaurant.findAll();
+    res.json(allRestaurants);
+})
+    
 
 
 app.listen(port, () => {
