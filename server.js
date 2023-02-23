c60163662872ec3063c0864e7eeae017084b5263
Mia Jimenez <miajimenez@expediagroup.com>
2023-02-23 14:55:42 +0000

    add router for restaurants

--- server.js
+++ server.js
@@ -2,49 +2,13 @@ const express = require("express");
 const app = express();
 const {Restaurant} = require("./models/index")
 const {sequelize} = require("./db");
+const restaurant = require("./routers/restaurantsRouter");
 
 const port = 3000;
 
-//TODO: Create your GET Request Route Below: 
-app.get('/restaurants', async (req, res) => {
-    const allRestaurants = await Restaurant.findAll();
-    res.json(allRestaurants);
-});
-
-// PT2 GET request for "/restaurants/:id"
-app.get('/restaurants/:id', async (req, res) => {
-    const restaurant = await Restaurant.findByPk(req.params.id);
-    res.json(restaurant)
-})
-
-// PT3 create express routes
-app.use(express.json())
-app.post('/restaurants', async (req, res) => {
-    req.body = {name: "Vapiano", location: "Covent Garden", cuisine: "Italian"}
-    const newRestaurant = req.body;
-    const createNewRest = await Restaurant.create(newRestaurant);
-    const allRestaurants = await Restaurant.findAll()
-    res.json(allRestaurants);
-})
-
-app.use(express.json())
-app.put('/restaurants/:id', async (req, res) => {
-    let id = req.params.id;
-    req.body = {name: "Nandos", location: "London", cuisine: "Brazilian"}
-    const newRestaurant = req.body;
-    const updateRestaurant = await Restaurant.update(newRestaurant, { where: { id: id } });
-    const allRestaurants = await Restaurant.findAll();
-    res.json(allRestaurants);
-})
-
-app.use(express.json())
-app.delete('/restaurants/:id', async (req, res) => {
-    let id = req.params.id;
-    const deleteRestaurant = await Restaurant.destroy({ where: { id: id } });
-    const allRestaurants = await Restaurant.findAll();
-    res.json(allRestaurants);
-})
-    
+app.use(express.json());
+app.use('/restaurants', restaurant);
+app.use('/restaurants/:id', restaurant);
 
 
 app.listen(port, () => {
